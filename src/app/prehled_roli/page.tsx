a393f9e792fa3f89455f1b0e3c2d9c58a14eb2b2
Jan Cinybulk <xcinda@gmail.com>
2025-08-20 19:09:27 +0200

    testing role overview and routing 2

---
+++ src/app/prehled_roli/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+  
+ const role = await db.query.opRole.findMany();
+ console.log(role);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-8">{
+          role.map((role,index) => (
+          <div key={index}
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+          >
+            <h3 className="text-2xl font-bold">{role.nazevRole}</h3>
+            <div className="text-lg">
+              {role.poznamka}
+            </div>
+            <div className="flex flex-col"> <button>Přidělit přístup</button></div>
+          </div>))
+          }
+          <div
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+          >
+            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <div className="text-lg">
+              Learn more about Create T3 App, the libraries it uses, and how to
+              deploy it.
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
