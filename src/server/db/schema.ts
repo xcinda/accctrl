15f41ff150ffa419e190e6ad662b634e4d9e4005
Jan Cinybulk <xcinda@gmail.com>
2025-08-20 14:12:55 +0200

    init

---
+++ src/server/db/schema.ts
@@ -0,0 +1,27 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import { index, mysqlTableCreator } from "drizzle-orm/mysql-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = mysqlTableCreator((name) => `accctrl_${name}`);
+
+export const posts = createTable(
+  "post",
+  (d) => ({
+    id: d.bigint({ mode: "number" }).primaryKey().autoincrement(),
+    name: d.varchar({ length: 256 }),
+    createdAt: d
+      .timestamp()
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp().onUpdateNow(),
+  }),
+  (t) => [index("name_idx").on(t.name)],
+);
