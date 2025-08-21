38aedd60eb7ee3ebf8f79bfb6917f1fdb965189e
Jan Cinybulk <xcinda@gmail.com>
2025-08-21 18:53:44 +0200

    moved role list to main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@ import Link from "next/link";
 import { UserList,Selector } from "~/components";
 import { db } from "~/server/db";
 
+export const user="jcinybulk";
+
 export default async function HomePage() {
+
+  const userRole = await db.query.opHlavni.findMany({
+    where: (userRole, { eq }) => eq(userRole.login, user),
+});
+  const role = await db.query.opRole.findMany();
 
   const users = ['user1','user2','user3','user4']
 
   return (
-      <div className="flex flex-row justify-between h-full w-7/10 overflow-hidden">
-        <div className="w-1/4 h-full overflow-hidden flex flex-col">
+      <div className="flex flex-row justify-between h-full w-7/10 overflow-hidden pt-10">
+        <div id="LevySloupec" className="w-1/4 h-full overflow-hidden flex flex-col">
+          <div className="w-full text-center text-xl font-bold pb-3">{user}</div>
           <div className="overflow-scroll flex-1">
             <UserList users={users}/>
           </div>
@@ -17,7 +25,23 @@ export default async function HomePage() {
           </div>
         </div>
           <div className="w-full h-full overflow-scroll">
-            CONTENT HERE andherererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererreerererererre
+            <div className="flex flex-col w-full items-center overflow-scroll">
+        <div id="PravySloupec" className="flex flex-row justify-between w-7/10">
+          <div className="flex flex-col gap-4 w-full justify-center">{
+            userRole.map((role,index) => (
+            <div key={index}
+              className="flex flex-row justify-around items-center rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            >
+              <h3 className="text-2xl font-bold">{role.idRole}</h3>
+              <div className="text-lg">
+                {role.datumVykonZrizeni}
+              </div>
+              <div className="flex flex-col"> <button>Přidělit přístup</button></div>
+            </div>))
+            }
+          </div>
+        </div>
+      </div>
           </div>
       </div>
   );
